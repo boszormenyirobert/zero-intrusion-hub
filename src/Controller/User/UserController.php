39262c724d8e2a89e99978fc5a1fe35c4a3721ae
r62g8y <boszormenyirobert@yahoo.com>
2025-10-19 17:13:13 +0200

    debug

--- src/Controller/User/UserController.php
+++ src/Controller/User/UserController.php
@@ -30,12 +30,12 @@ class UserController extends AbstractController
         ) {  
 
         $token = $csrfTokenManager->getToken('userLoginCsrf')->getValue();
-        dd($token);
+        
         $userPublicId = null;
         if ($request->query->has('userPublicId')) {
             $userPublicId = $request->query->get('userPublicId');
         }
-
+dd($userPublicId);
         return $this->render('views/users/user-login.html.twig', [
             'authentication' => $this->userService->getQrCode('user_login', [],  $userPublicId),
             'userLoginCsrf' => $token,
