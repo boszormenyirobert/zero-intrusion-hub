6cfa07cb8b1fc1365b16466673be5469b796f367
r62g8y <boszormenyirobert@yahoo.com>
2025-10-19 17:14:56 +0200

    debug

--- src/Controller/User/UserService.php
+++ src/Controller/User/UserService.php
@@ -35,7 +35,7 @@ class UserService
             'hmac'     => $hmac,
         ] = $this->getPublicIdDomainHmac($corporateIdentification);
 
-        dd($corporateIdentification);
+        dd($publicId,$domain, $hmac);
 
 
         $response = $this->authorizationControllService->getSecurePostRequest([
