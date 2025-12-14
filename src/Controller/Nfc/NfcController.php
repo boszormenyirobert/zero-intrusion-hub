ef69fe84dda783465fa59e5e35d80c55cd733bb2
boszormenyirobert <boszormenyirobert@yahoo.com>
2025-12-14 15:21:37 +0100

    add nfc controller

---
+++ src/Controller/Nfc/NfcController.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Controller\Nfc;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+use App\Controller\User\UserService;
+use Psr\Log\LoggerInterface;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Security\Csrf\CsrfTokenManagerInterface;
+use Symfony\Component\Security\Csrf\CsrfToken;
+use App\Repository\UserRepository;
+use Symfony\Component\HttpFoundation\Cookie;
+use Lexik\Bundle\JWTAuthenticationBundle\Services\JWTTokenManagerInterface;
+use App\DTO\RegistrationProcessDTO;
+use Symfony\Component\HttpFoundation\Response;
+use Lexik\Bundle\JWTAuthenticationBundle\Encoder\JWTEncoderInterface;
+use App\Service\JWT\JwtService;
+
+class NfcController extends AbstractController
+{
+    public function __construct(
+        private LoggerInterface $logger,
+        private JWTEncoderInterface $jwtEncoder,
+        private UserService $userService,
+        JwtService $jwtService,
+        private UserRepository $userRepository,
+    ) {}
+
+    #[Route('/api/nfc/users', name: 'api_nfc_users', methods: "GET")]
+    public function getNfcUsers(
+        Request $request,
+        JwtService $jwtService
+        ) {
+            $response = ['users' => ['boszormenyirobert@yahoo.com','vilagteteje@freemail.hu']];
+
+            return $this->json($response);
+        }
+}
